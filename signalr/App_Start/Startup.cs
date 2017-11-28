f711ea81c8027752b18a9868657fe5a0b4ee08fe
IRON-MAN\ylmazbarscan <ylmazbarscan@outlook.com>
2017-11-29 00:56:58 +0300

    Initial commit

---
+++ signalr/App_Start/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(signalr.App_Start.Startup))]
+
+namespace signalr.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ signalr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="signalr.MvcApplication" Language="C#" %>
